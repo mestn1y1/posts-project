171ca639abf3f84fe1ffa8183e70e8f867fcae52
mestn1y <mestn1y@hotmail.com>
2025-08-22 00:48:14 +0300

    change DBfor local,create submit component and adding function to createPoat component

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,8 +1,27 @@
+import FormSubmit from "@/components/form-submit";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
 export default function NewPostPage() {
+  async function createPost(formData) {
+    "use server";
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+
+    await storePost({
+      imageUrl: "",
+      title,
+      content,
+      userId: 1,
+    });
+    redirect("/feed");
+  }
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
@@ -21,8 +40,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
